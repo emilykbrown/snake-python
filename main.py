40235801692a15e5a63290385216e6339c21a730
emilykbrown <disabledsarcasm@gmail.com>
2025-03-23 16:42:16 -0500

    Move

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ WINDOW_HEIGHT = TILE_SIZE * ROWS
 
 # Tile
 class Tile:
-    def __init__(self, x, y)
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
@@ -35,11 +35,51 @@ screen_height = window.winfo_screenheight()
 window_x = int((screen_width/2) - (window_width/2))
 window_y = int((screen_height/2) - (window_height/2))
 # (w)x(h)+(x)+(y)
-window.geometry(f"{window_width}x{window_height}+{window_x}+{window_Y}")
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 # Initialize game
-snake = Tile(5*TILE_SIZE, 5*TILE_SIZE) # snake head
+snake = Tile(5*TILE_SIZE, 5*TILE_SIZE) # snake tile
+food = Tile(10*TILE_SIZE, 10*TILE_SIZE) # food til
+velocityX = 0
+velocityY = 0
 
-def draw()
+# Key directions
+def change_direction(e):
+    global velocityX, velocityY
+    
+    if (e.keysym == "Up"):
+        velocityX = 0
+        velocityY = -1
+    elif (e.keysym == "Down"):
+        velocityX = 0
+        velocityY = 1
+    elif (e.keysym == "Left"):
+        velocityX = -1
+        velocityY = 0
+    elif (e.keysym == "Right"):
+        velocityX = 1
+        velocityY = 0
+        
+def move():
+    global snake
+    
+    snake.x += velocityX * TILE_SIZE
+    snake.y += velocityY * TILE_SIZE
+    
 
+def draw():
+    global snake
+    move()
+        
+    # Snake   
+    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "lime green")
+    
+    # Food
+    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill = "red")
+    
+    window.after(100, draw)
+
+draw()
+
+window.bind("<KeyRelease>", change_direction)
 window.mainloop()
