c9bc2f0b3b0ea052a7a8213e9236b8c6031dc0b7
emilykbrown <disabledsarcasm@gmail.com>
2025-03-23 19:26:59 -0500

    Done

--- main.py
+++ main.py
@@ -42,40 +42,84 @@ snake = Tile(5*TILE_SIZE, 5*TILE_SIZE) # snake tile
 food = Tile(10*TILE_SIZE, 10*TILE_SIZE) # food til
 velocityX = 0
 velocityY = 0
+snake_body = []
+game_over = False
+score = 0
 
 # Key directions
 def change_direction(e):
     global velocityX, velocityY
     
-    if (e.keysym == "Up"):
+    if (game_over):
+        return
+    
+    if (e.keysym == "Up" and velocityY != 1):
         velocityX = 0
         velocityY = -1
-    elif (e.keysym == "Down"):
+    elif (e.keysym == "Down" and velocityY != -1):
         velocityX = 0
         velocityY = 1
-    elif (e.keysym == "Left"):
+    elif (e.keysym == "Left" and velocityX != 1):
         velocityX = -1
         velocityY = 0
-    elif (e.keysym == "Right"):
+    elif (e.keysym == "Right" and velocityX != -1):
         velocityX = 1
         velocityY = 0
         
 def move():
-    global snake
+    global snake, food, snake_body, game_over
+    if (game_over):
+        return
     
+    if (snake.x < 0 or snake.x >= WINDOW_WIDTH or snake.y < 0 or snake.y >= WINDOW_HEIGHT):
+        game_over = True
+        return
+    
+    for tile in snake_body:
+        if (snake.x == tile.x and snake.y == tile.y):
+            game_over = True
+            return
+
+    if (snake.x == food.x and snake.y == food.y):
+        snake_body.append(Tile(food.x, food.y))
+        food.x = random.randint(0, COLS-1) * TILE_SIZE
+        food.y = random.randint(0, ROWS-1) * TILE_SIZE
+        
+    # move snake body
+    for i in range(len(snake_body)-1, -1, -1):
+        tile = snake_body[i]
+        if (i == 0):
+            tile.x = snake.x
+            tile.y = snake.y
+        else:
+            prev_tile = snake_body[i-1]
+            tile.x = prev_tile.x
+            tile.y = prev_tile.y
+
     snake.x += velocityX * TILE_SIZE
     snake.y += velocityY * TILE_SIZE
     
 
 def draw():
-    global snake
+    global snake, food, snake_body, game_over, score
     move()
-        
-    # Snake   
+    
+    canvas.delete("all")
+    
+    # Draw food
+    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill = "red")
+    
+    # Draw snake   
     canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "lime green")
     
-    # Food
-    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill = "red")
+    # Draw snake body
+    for tile in snake_body:
+        canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill = "lime green")
+    
+    if (game_over):
+        canvas.create_text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, font = "Arial 20", text = f"Game Over: {score}", fill = "white")
+    else:
+        canvas.create_text(30, 20, font = "Arial 10", text = f"Score {score}", fill = "white")
     
     window.after(100, draw)
 
